c59984f04c4d0bead527af15cbf2fc5c8b7e730e
Philip Mark Gutierrez <sensei_slayer@protonmail.com>
2026-09-04 19:40:43 +0800

    init code

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -2,18 +2,24 @@ import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { providePrimeNG } from 'primeng/config';
+
+import Aura from '@primeuix/themes/aura';
+import { environment } from './environment/environment';
+import { BlogModule } from './blog/blog-module';
 
 @NgModule({
-  declarations: [
-    App
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [App],
+  imports: [BrowserModule, AppRoutingModule, BlogModule],
   providers: [
     provideBrowserGlobalErrorListeners(),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+      },
+      license: environment.primeUILicense,
+    }),
   ],
-  bootstrap: [App]
+  bootstrap: [App],
 })
-export class AppModule { }
+export class AppModule {}
